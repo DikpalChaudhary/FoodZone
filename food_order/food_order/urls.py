41d54b9dff32efb5e63f03df88ad11c93ad44809
DikpalChaudhary <cdikpal747@gmail.com>
2025-07-26 15:19:52 +0545

    updated

--- food_order/food_order/urls.py
+++ food_order/food_order/urls.py
@@ -16,10 +16,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('', include('myApp.urls')), 
-    path('admin/',include('my_admin.urls'))
+    path('admin/', include('my_admin.urls')),
+
+    # For serving media files when DEBUG = False
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
+# Automatically serve media files in DEBUG mode
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
